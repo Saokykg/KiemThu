75ff15870a5d3fecfb4d3f69e322b76a32ad9e6d
saokykg <1851050051hoa@ou.edu.vn>
2021-05-01 12:21:45 +0700

    doimk

--- Source/QuanLySV/QuanLySv/src/MainSource/TaiKhoanController.java
+++ Source/QuanLySV/QuanLySv/src/MainSource/TaiKhoanController.java
@@ -6,8 +6,17 @@
 package MainSource;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import quanly.Class.Account;
 
 /**
  * FXML Controller class
@@ -16,12 +25,58 @@ import javafx.fxml.Initializable;
  */
 public class TaiKhoanController implements Initializable {
 
+    
+    
+    @FXML
+    private Text txtUser;    
+    @FXML
+    private Text txtPermission;    
+    @FXML
+    private TextField txtOldPas;    
+    @FXML
+    private TextField txtNewPas;    
+    @FXML
+    private TextField txtConfirm;
+    @FXML
+    private Button btnConfirm;
     /**
      * Initializes the controller class.
      */
+    Account acc;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        try {
+            acc = Utils.getAccount(Login.loginid);
+        } catch (SQLException ex) {
+            Logger.getLogger(TaiKhoanController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.txtUser.setText(acc.getUsername());
+        this.txtPermission.setText(acc.getLoaitk());
     }    
+    
+    protected void canhcao(String txt){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText(txt);
+        alert.show();
+        this.txtOldPas.clear();
+        this.txtNewPas.clear();
+        this.txtConfirm.clear();    
+    }
+    
+    public void btnClicked() throws SQLException{
+        if (this.txtOldPas.getText().equals(acc.getPassword())){
+            if (this.txtConfirm.getText().equals(this.txtNewPas.getText())){
+                Utils.updateAccount(Login.loginid, this.txtNewPas.getText());
+                canhcao("Doi password thanh cong!!!");
+            }
+            else{
+                canhcao("Mat khau nhap lai khong chinh xac!!!");
+            }
+        }
+        else{
+            canhcao("Mat khau khong chinh xac!!!");
+        }
+    }
     
 }
--- Source/QuanLySV/QuanLySv/src/MainSource/Utils.java
+++ Source/QuanLySV/QuanLySv/src/MainSource/Utils.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javafx.util.Pair;
+import quanly.Class.Account;
 import quanly.Class.Hocki;
 import quanly.Class.Monhoc;
 import quanly.Class.Sinhvien;
@@ -296,5 +297,24 @@ public class Utils {
             stm.setInt(2, idsv);
             stm.executeUpdate();
         }
+    }
+
+    public static Account getAccount(int id) throws SQLException {
+        String sql = "select * from account where id_account = ?";
+        Connection conn = jdbcUtils.getConn();
+        PreparedStatement stm = conn.prepareStatement(sql);
+        stm.setInt(1, id);
+        ResultSet rs = stm.executeQuery();
+        rs.next();
+        return new Account(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4));
+    }
+
+    static void updateAccount(int id, String pass) throws SQLException {
+        String sql = "update account set mat_khau = ? where id_account = ? ";
+        Connection conn = jdbcUtils.getConn();
+        PreparedStatement stm = conn.prepareStatement(sql);
+        stm.setString(1, pass);
+        stm.setInt(2, id);
+        stm.executeUpdate();
     }
 }
